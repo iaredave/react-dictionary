a1c198077ee66a6e68ea74912324465be44a98a1
David Goulet <gouletdavid95@gmail.com>
2019-09-07 14:41:01 -0400

    Updates

---
+++ precache-manifest.ec789306daf0cfe063cd45dbb7c1f988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8635fb622a3e53a3ee28e6b5698660a",
+    "url": "/react-dictionary/index.html"
+  },
+  {
+    "revision": "7a0bbac055ec9eb6592c",
+    "url": "/react-dictionary/static/css/main.643af243.chunk.css"
+  },
+  {
+    "revision": "1c8ed6f1d70e461c4860",
+    "url": "/react-dictionary/static/js/2.22abd9cb.chunk.js"
+  },
+  {
+    "revision": "7a0bbac055ec9eb6592c",
+    "url": "/react-dictionary/static/js/main.e1239395.chunk.js"
+  },
+  {
+    "revision": "0670d23d0b17015598bb",
+    "url": "/react-dictionary/static/js/runtime~main.c3d1d514.js"
+  }
+]);
